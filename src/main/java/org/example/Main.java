1593f5a11ccc6b910e52577d6156cc9bbd5fa646
Mark-progg <mark.mishutin@yandex.ru>
2022-12-10 19:35:42 +0300

    Final commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,29 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите дату в формате dd.MM.yyyy:");
+
+        String inputDate = sc.nextLine();
+
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        Date date = format.parse(inputDate);
+        String inputYear = new SimpleDateFormat("yyyy").format(date);
+        GregorianCalendar cal = new GregorianCalendar();
+
+        if (cal.isLeapYear(Integer.parseInt(inputYear)))
+            System.out.println("Високосный год");
+        else
+            System.out.println("Не високный год");
     }
 }
